7a4f4cf40f9de1eb2528bc8847bac7dca0fa2eeb
SwarnalataJena <jenaswarnalata600@gmail.com>
2022-09-16 21:00:16 +0530

    all updated today

---
+++ src/test/java/makeMyTripTC/Test1.java
@@ -0,0 +1,42 @@
+package makeMyTripTC;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	public static void main(String[] args) throws Throwable {
+		Date d=new Date();
+		String str = d.toString();
+		String[] s = str.split(" ");
+		String day = s[0];
+		String month=s[1];
+		String date=s[2];
+		String year=s[5];
+		String current_date=day+" "+month+" "+date+" "+year;
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));		
+		driver.get("https://www.makemytrip.com/");
+
+		driver.switchTo().frame("webklipper-publisher-widget-container-notification-frame");
+		driver.findElement(By.className("close")).click();
+		driver.findElement(By.xpath("//span[@class='langCardClose']")).click();
+
+		driver.findElement(By.id("fromCity")).sendKeys("banglore");
+		driver.findElement(By.xpath("//div[text()='BLR']")).click();
+
+		driver.findElement(By.id("toCity")).sendKeys("new york");
+		driver.findElement(By.xpath("//div[text()='NYC']")).click();
+
+		Thread.sleep(3000);		
+		driver.findElement(By.xpath("//div[@aria-label='"+current_date+"']")).click();
+//		driver.findElement(By.xpath("//div[@aria-label='Fri Sep 16 2022']")).click();
+	}
+
+}
---
+++ src/test/java/makeMyTripTC/Test2.java
@@ -0,0 +1,42 @@
+package makeMyTripTC;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void testcase2()
+	{
+				WebDriver driver=new ChromeDriver();
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));		
+				driver.get("https://www.makemytrip.com/");
+				
+				driver.switchTo().frame("webklipper-publisher-widget-container-notification-frame");
+				driver.findElement(By.className("close")).click();
+				driver.findElement(By.xpath("//span[@class='langCardClose']")).click();
+				
+				driver.findElement(By.id("fromCity")).sendKeys("banglore");
+				driver.findElement(By.xpath("//div[text()='BLR']")).click();
+				
+				driver.findElement(By.id("toCity")).sendKeys("new york");
+				driver.findElement(By.xpath("//div[text()='NYC']")).click();
+				
+				for(int i=0;i<12;i++)
+				{
+					try {
+						driver.findElement(By.xpath("//div[@aria-label='Wed Feb 08 2023']")).click();
+					} catch (Exception e) {
+						driver.findElement(By.xpath("//span[@class='DayPicker-NavButton DayPicker-NavButton--next']")).click();
+					}
+				}
+				
+			}
+	
+
+}
